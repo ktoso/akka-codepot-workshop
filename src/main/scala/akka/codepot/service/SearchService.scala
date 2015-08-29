23eca5632f5faaa214729b627af0515ef1e1588d
Konrad Malawski <konrad.malawski@typesafe.com>
2015-08-29 03:02:50 +0200

    streaming responses

--- src/main/scala/akka/codepot/service/SearchService.scala
+++ src/main/scala/akka/codepot/service/SearchService.scala
@@ -5,10 +5,11 @@ import akka.codepot.engine.search.SearchMaster
 import akka.codepot.engine.search.tiered.TieredSearchProtocol.{Search, SearchResults}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport
+import akka.http.scaladsl.model.HttpEntity.Chunked
+import akka.http.scaladsl.model.{ContentTypes, HttpResponse}
 import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
-import akka.util.Timeout
-import spray.json.DefaultJsonProtocol
+import akka.util.{ByteString, Timeout}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -24,14 +25,17 @@ trait SearchService extends Directives with ScalaXmlSupport
 
   lazy val searchMaster: ActorRef = system.actorOf(SearchMaster.props(), "searchMaster")
 
-  import DefaultJsonProtocol._
-  implicit val results = jsonFormat1(SearchResults)
-
   def searchRoutes =
     pathPrefix("search") {
       get {
         parameters('q, 'n ? 100) { (q, max) =>
-          complete { search(q, max) }
+          complete {
+            search(q, max).map { r =>
+              HttpResponse(entity =
+                Chunked.fromData(ContentTypes.`application/json`,
+                chunks = r.source.map(k => ByteString(s"""{ "keyword": "$k" }\n"""))))
+            }
+          }
         }
       } ~
       complete {
@@ -39,6 +43,7 @@ trait SearchService extends Directives with ScalaXmlSupport
           <h1>Say hello to akka-http</h1>
           <form action="/search">
             <input name="q"></input>
+            <input name="max"></input> (nr plz)
             <button value="Search!"/>
           </form>
         </div>
